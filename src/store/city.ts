bf2a6c8e5a8cadedc84eadeb1b4b16ceca5fc15d
Nikita Makarin <n-makarin@mail.ru>
2019-12-12 23:01:37 +0300

    Add getCoordinates & getCityNameByCoordinates src\store\city.ts

--- src/store/city.ts
+++ src/store/city.ts
@@ -1,10 +1,14 @@
 import * as Types from '@/types/store/city'
 import VueGeolocation from 'vue-browser-geolocation'
+import googleMaps from '@/api/google-maps'
+import getCity from '@/utils/google-maps/get-city'
+
 const geolocation: any = VueGeolocation
 
 export default {
   namespaced: true,
   state: {
+    data: '',
     coordinates: {
       lat: 0,
       lng: 0
@@ -13,16 +17,37 @@ export default {
   mutations: {
     SET_COORDINATES (store: any, coordinates: Types.Coordinates) {
       store.coordinates = coordinates
+    },
+    SET (store: any, city: string) {
+      store.data = city
     }
   },
   actions: {
-    getCoordinates ({ commit }: any): void {
-      geolocation.getLocation().then((coordinates: Types.Coordinates) => {
-        commit('SET_COORDINATES', coordinates)
+    /**
+     * Get current user coordinates
+     * and set them to store
+     */
+    async getCoordinates ({ commit }: any): Promise<void> {
+      await geolocation.getLocation().then((coordinates: Types.Coordinates): void => {
+        const coords: Types.Coordinates = {
+          lat: coordinates.lat,
+          lng: coordinates.lng
+        }
+        commit('SET_COORDINATES', coords)
       })
+    },
+    /**
+     * Get city name by coordinates from store
+     */
+    async getCityNameByCoordinates ({ commit, dispatch, getters }: any): Promise<void> {
+      await dispatch('getCoordinates')
+      const rawData = await googleMaps.getData(getters.coordinates)
+      const city: string = getCity(rawData)
+      commit('SET', city)
     }
   },
   getters: {
+    data: (state: any) => state.data,
     coordinates: (state: any) => state.coordinates
   },
   modules: {
