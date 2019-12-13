0101b84250ab35490a13ab316167a8f9607e066e
Nikita Makarin <n-makarin@mail.ru>
2019-12-13 16:02:53 +0300

    Create src\components\views\weather.vue scaffolds

---
+++ src/components/views/weather.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="weather">
+    <div class="weather__description">
+      {{ description }}
+    </div>
+    <div class="weather__rain">
+      {{ rain }}
+    </div>
+    <div class="weather__snow">
+      {{ snow }}
+    </div>
+    <div class="weather__temp">
+      <div class="weather__feels-like">
+        {{ temp.feelsLike }}
+      </div>
+      <div class="weather__tepm">
+        {{ temp.tepm }}
+      </div>
+    </div>
+    <div class="weather__wind">
+      <div class="weather__feels-speed">
+        {{ wind.speed }}
+      </div>
+      <div class="weather__dir">
+        {{ wind.dir }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    description: {
+      type: String,
+      default: ''
+    },
+    rain: {
+      type: Boolean,
+      default: false
+    },
+    snow: {
+      type: Boolean,
+      default: false
+    },
+    temp: {
+      type: Object,
+      default: () => {}
+    },
+    wind: {
+      type: Object,
+      default: () => {}
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+</style>
