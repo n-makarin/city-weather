1c7dc2eb27c16b23d3e84c46b3fcdc2383449485
Nikita Makarin <n-makarin@mail.ru>
2019-12-13 17:28:16 +0300

    Remove 'vue-autocomplete'

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vSelect from 'vue-select'
-
-Vue.component('v-select', vSelect)
 
 Vue.config.productionTip = false
 
