9094235ca6246326166fd2c8f958b6139be99d44
Nikita Makarin <n-makarin@mail.ru>
2019-12-13 18:14:13 +0300

    Create src\components\views\select-city.vue

---
+++ src/components/views/select-city.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="select-city">
+    <VueGooglePlaces
+      class="select-city__input"
+      api-key="AIzaSyCekJlCBYHrhLO_hg500FOVcZQ4_iBoosI"
+      types="(cities)"
+      placeholder="Input your place"
+      @placechanged="getAddressData"
+      @noresult="onNoResult"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {
+  },
+  data () {
+    return {
+      address: ''
+    }
+  },
+  watch: {
+    address (newValue) {
+      this.$store.dispatch('city/set', this.address)
+    }
+  },
+  methods: {
+    /**
+    * When the location found
+    * @param {Object} addressData Data of the found location
+    * @param {Object} placeResultData PlaceResult object
+    * @param {String} id Input container ID
+    */
+    getAddressData (addressData: any, placeResultData: any, id: any) {
+      this.address = addressData.locality
+    },
+    onNoResult () {
+      this.address = ''
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+.select-city {
+
+  &__input {
+    input {
+      width: 300px;
+      padding: 5px;
+    }
+  }
+}
+</style>
